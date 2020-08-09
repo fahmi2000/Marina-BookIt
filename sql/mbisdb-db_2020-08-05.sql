033bd243d1d95cd5ee790828fa13adda10144a4b
IHAZNUGGETZ\ihazn <fahmialiff00@gmail.com>
2020-08-09 21:44:31 +0800

    lupa nak update SQL

--- sql/mbisdb-db_2020-08-05.sql
+++ sql/mbisdb-db_2020-08-05.sql
@@ -1,14 +1,13 @@
 -- phpMyAdmin SQL Dump
--- version 4.9.2
+-- version 5.0.2
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1:3306
--- Generation Time: Aug 05, 2020 at 04:04 AM
+-- Generation Time: Aug 09, 2020 at 02:43 PM
 -- Server version: 8.0.18
--- PHP Version: 7.4.0
+-- PHP Version: 7.4.5
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-SET AUTOCOMMIT = 0;
 START TRANSACTION;
 SET time_zone = "+00:00";
 
@@ -29,19 +28,26 @@ SET time_zone = "+00:00";
 --
 
 CREATE TABLE `users` (
-  `userID` int(11) NOT NULL,
+  `userID` int(255) NOT NULL,
+  `userType` varchar(20) NOT NULL,
   `userName` tinytext NOT NULL,
-  `userEmail` tinytext NOT NULL,
+  `userEmail` varchar(50) NOT NULL,
   `userPwd` longtext NOT NULL,
-  `userType` int(3) NOT NULL
+  `fName` varchar(50) DEFAULT NULL,
+  `lName` varchar(50) DEFAULT NULL,
+  `phoneNumber` varchar(50) DEFAULT NULL,
+  `userGender` varchar(50) DEFAULT NULL,
+  `emailVerify` varchar(5) CHARACTER SET utf8 COLLATE utf8_general_ci DEFAULT '0'
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 --
 -- Dumping data for table `users`
 --
 
-INSERT INTO `users` (`userID`, `userName`, `userEmail`, `userPwd`, `userType`) VALUES
-(1, 'aliff', 'fahmialiff00@gmail.com', '$2y$10$Kh.QWrHvPkZUYIip08tttuymNY4tAMTLn5wwLFCzfnpIEpDJQDdey', 1);
+INSERT INTO `users` (`userID`, `userType`, `userName`, `userEmail`, `userPwd`, `fName`, `lName`, `phoneNumber`, `userGender`, `emailVerify`) VALUES
+(1, '1', 'aliff', 'fahmialiff00@gmail.com', '$2y$12$Gj8Z4W1BT1XsxKyASkAJYuxn4HNCFq3umCIM5QpSKkKeny7Uct4Iu', 'Muhamad', 'Aliff', '0182329739', 'maleGender', ''),
+(2, '2', 'fahmi', 'alifffahmi00@gmail.com', '$2y$10$wl9SdRqoZC7xOxHTBBQ.YePtV68Dl.E36z9chvKZtkiaVLqOM2zQi', 'Fahmi', 'Aliff', NULL, 'femaleGender', ''),
+(3, '3', 'adam', 'adam@gmail.com', '$2y$10$pdmWwyQw6hh3IkiMOliIVeSnq5i9BVoSuiiwDYRT0nibCOXQHPrc6', 'Adam', 'Shah', '0383123456', 'shyGender', '');
 
 --
 -- Indexes for dumped tables
@@ -61,7 +67,7 @@ ALTER TABLE `users`
 -- AUTO_INCREMENT for table `users`
 --
 ALTER TABLE `users`
-  MODIFY `userID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `userID` int(255) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
